c216afe541212a5e7a6408e03f9fe8955bc4a2b1
Véro Grué <veronique.gru@educa.jcyl.com>
2025-11-17 07:25:53 +0100

    primer commit

---
+++ indexProyectoTema5.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Véro Grué - DWESProyectoTema5</title>
+        <!--Fuente de google font-->
+        <!--Para descargar iconos. https://v2.boxicons.com/usage  (import the css)-->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="webroot/css/styles.css">
+        <!--https://cdnjs.com/libraries/font-awesome --> 
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <header class="header">
+            <a href="../VGDWESProyectoDWES/indexProyectoDWES.html">volver</a>
+            <h1>PROYECTO TEMA 5</h1>
+        </header>
+        <main>
+            <section>
+                <h2>EJERCICIOS DEL TEMA 5</h2>
+
+                <h3>CONTENIDO DE LOS FICHEROS EXTERNOS</h3>
+                <table>
+                    <tr>
+                        <th>Descriptción</th>
+                        <th>Mostrar</th>
+                        <th>Descriptción</th>
+                        <th>Mostrar</th>
+                    </tr>
+                    <tr>
+                        <td class="texto">Creación de la base de Datos y del usuario. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraCreaDBVGDWESProyectoTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="texto">Librería personal. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraMiLibreriaStatic.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                    </tr>
+
+                    <tr>
+
+                        <td class="texto">Carga de la base de Datos. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraCargaDBVGDWESProyectoTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="texto">Fichero de configuración PDO. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraconfDBPDO.php"><i class="fa-solid fa-eye"></i> </a></td>
+                    </tr>
+                    <tr>
+
+                        <td class="texto">Borrado de la base de Datos. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraBorraDBVGDWESProyectoTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="texto"> </td>
+                        <td class="iconos"> <a href=""><!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+
+
+                    </tr>
+
+
+                </table>
+
+                <table>
+                    <tr>
+                        <th rowspan="2">Nº</th>
+                        <th rowspan="2">Enunciados</th>
+                        <th colspan="2">PDO</th>
+                    </tr>
+                    <tr>
+                        <th>Ejecutar</th>
+                        <th>Mostrar</th>
+                    </tr>
+                    <tr>
+                        <th>0</th>
+<!--                    <span class="span">text</span> -->
+                    <td class="texto">Mostrar el contenido de las variables superglobales y phpinfo(). </td>
+                    <td class="iconos"><a href="codigoPHP/ejercicio00.php"><i class="fa-solid fa-circle-play"> </i> </a>  </td>
+                    <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio00.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                   
+                    </tr>
+
+                    <tr>
+                        <th>1</th>
+                        <!-- <span class="span">text</span> -->
+
+                        <td class="texto"> Desarrollo de un control de acceso con identificación del usuario basado en la función header().</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio01.php"><i class="fa-solid fa-circle-play"> </i> </a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio01.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                    </tr>
+                    <tr>
+                        <th>2</th>
+                        <td class="texto">Desarrollo de un control de acceso con identificación del usuario basado en la función header() y
+                            en el uso de una tabla “Usuario” de la base de datos. (PDO).</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio02.php"><i class="fa-solid fa-circle-play"></i></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio02.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                    </tr>
+                    <tr>
+                        <th>3</th>
+                        <td class="texto">Desarrollo de una aplicación (Proyecto LoginLogoffTema5) con control de acceso e identificación
+                            del usuario basado en un formulario (Login.php).</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio03.php"><!--<i class="fa-solid fa-circle-play"></i>--></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio03.php"><!--<i class="fa-solid fa-eye"></i>--></a>  </td>
+
+                    </tr>
+                    <tr>
+                        <th>4</th>
+                        <td class="texto">Implantación uso y disfrute de Xdebug en nuestro entorno de desarrollo. <span class="span">(Instalado en Ubuntu
+                                Server junto a Apache y lo usamos desde Windows integrándolo en NetBeans)</span>.</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio04.php"><!--<i class="fa-solid fa-circle-play"></i>--></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio04.php"><!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+                    </tr>
+                    <tr>
+                        <th>5</th>
+                        <td class="texto">Incorporar el control de acceso, identificación de usuario, cookies y sesiones a nuestro
+                            Mantenimiento de Departamentos.</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio05.php"><!--<i class="fa-solid fa-circle-play"></i>--></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio05.php"><!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+                    </tr>
+                    <tr>
+                        <th>6</th>
+                        <td class="texto">Ampliar la funcionalidad de la aplicación: MtoDepartamentos y MtoUsuarios.</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio06.php"><!--<i class="fa-solid fa-circle-play"></i>--></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio06.php"><!--<i class="fa-solid fa-eye"></i>--></a>  </td>
+                    </tr>
+                </table>
+            </section>
+        </main>
+        <footer class="footer">
+            <div class="footerContent">
+                <div><p class="copyright">
+                        2025-26 IES LOS SAUCES. &#169;Todos los derechos reservados.</p> <address><a href="../index.html">Véronique Grué.</a> Fecha de Actualización :
+                        <time datetime="2025-11-14"></time> 14-11-2025 </address>
+                </div>
+
+            </div>
+
+        </footer>
+
+    </body>
+</html>
